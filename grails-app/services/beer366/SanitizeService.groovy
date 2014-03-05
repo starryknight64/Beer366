9442839b0f6ca213f6c7fed06776b6c7bf3a928f
Phillip <pdponzer@gmail.com>
2014-03-04 21:33:24 -0600

    Added time elapsed to sanitizeService and overwrote FriendlyUrlService with the latest from the seo-friendly-urls plugin repo

--- grails-app/services/beer366/SanitizeService.groovy
+++ grails-app/services/beer366/SanitizeService.groovy
@@ -1,5 +1,7 @@
 package beer366
 
+import groovy.time.*
+
 /**
  * SanitizeService
  * A service class encapsulates the core business logic of a Grails application
@@ -9,6 +11,7 @@ class SanitizeService {
     static transactional = true
 
     def sanitizeAll() {
+		def startTime = new Date()
         System.out.println("Sanitizing usernames...")
         def users = User.list()
         users.eachWithIndex{ user, i ->
@@ -24,8 +27,9 @@ class SanitizeService {
                 System.out.println( "$i/${users.size()} (${(i * 100 / users.size()).floatValue().round(1)}% done)" )
             }
         }
-        System.out.println("done!")
+        System.out.println("done in ${TimeCategory.minus(new Date(), startTime)}!")
 
+		startTime = new Date()
         System.out.println("Sanitizing brewery names...")
         def breweries = Brewery.list()
         breweries.eachWithIndex{ brewery, i ->
@@ -41,8 +45,9 @@ class SanitizeService {
                 System.out.println( "$i/${breweries.size()} (${(i * 100 / breweries.size()).floatValue().round(1)}% done)" )
             }
         }
-        System.out.println("done!")
+        System.out.println("done in ${TimeCategory.minus(new Date(), startTime)}!")
 
+		startTime = new Date()
         System.out.println("Sanitizing beer names...")
         def beers = Beer.list()
         beers.eachWithIndex{ beer, i ->
@@ -58,6 +63,6 @@ class SanitizeService {
                 System.out.println( "$i/${beers.size()} (${(i * 100 / beers.size()).floatValue().round(1)}% done)" )
             }
         }
-        System.out.println("done!")
+        System.out.println("done in ${TimeCategory.minus(new Date(), startTime)}!")
     }
 }
